b7097622f92f7b068a73e5ea5deedb1453ace8f5
Alexander Shagov <x48j35@gmail.com>
2017-02-16 15:18:15 +0300

    Add dynamic view templates

--- hello_world.rb
+++ hello_world.rb
@@ -6,15 +6,28 @@ class HelloWorld
     when '/'
       template = File.read("views/index.erb")
       content = ERB.new(template)
-      ['200', {"Content-Type" => 'text/html'}, [content.result]]
+      ['200', {"Content-Type" => 'text/html'}, [erb(:index)]]
     when '/advice'
-      ['200', {"Content-Type" => 'text/plain'}, [Advice.new.sample]]
+      ['200',
+        {"Content-Type" => 'text/html'},
+        [erb(:advice, message: Advice.new.sample)]
+      ]
     else
       [
         '404',
-        {"Content-Type" => 'text/plain', "Content-Length" => '13'},
-        ["404 Not Found"]
+        {"Content-Type" => 'text/html', "Content-Length" => '51'},
+        [erb(:not_found)]
       ]
     end
+  end
+
+  private
+
+  def erb(filename, local = {})
+    message = local[:message]
+    b = binding
+    path = File.expand_path("../views/#{filename}.erb", __FILE__)
+    content = File.read(path)
+    ERB.new(content).result(b)
   end
 end
